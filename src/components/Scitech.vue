4d7b9c437bf66659990ac123c7f25e1040adface
ZeroDawn0D <umang26112003@gmail.com>
2023-05-08 23:26:32 +0530

    added changes

--- src/components/Scitech.vue
+++ src/components/Scitech.vue
@@ -13,18 +13,29 @@ export default{
 				<router-link to = "/scitech/template">
 					<Card link = "/img/scitech/Decoherence.jpg" size = "300px"/>
 			    </router-link>
+
+			    <router-link to = "/scitech/bioblitz">
 				<Card link = "/img/scitech/BioBlitz.jpg" size = "300px"/>
+			</router-link>
+				<router-link to = "/scitech/chemenigma">
 				<Card link = "/img/scitech/Chemenigma.jpg" size = "300px"/>
+			</router-link>
+				<router-link to = "/scitech/enumeration">
 				<Card link = "/img/scitech/Enumeration.jpg" size = "300px"/>
+			</router-link>
+				<router-link to = "/scitech/quadsparks">
 				<Card link = "/img/scitech/Quadsparks.jpg" size = "300px"/>
+			</router-link>
+				<router-link to = "/scitech/bah">
 				<Card link = "/img/scitech/BAH.jpg" size = "300px"/>
+			</router-link>
+				<router-link to = "/scitech/pis">
 				<Card link = "/img/scitech/PIS.jpg" size = "300px"/>
+			</router-link>
+
 				<router-link to = "/scitech/exhibitions">
 					<Card link = "/img/scitech/Exhibitions.jpg" size = "300px"/>
 				</router-link>
-				<router-link to = "/scitech/PIS">
-					<Card link = "/img/scitech/PIS.jpg" size = "300px"/>
-			    </router-link>
 			</div>
 		</div>
 	</div>
