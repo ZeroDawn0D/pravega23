064083aad6f4442126f4de44b1c51842c58f93ff
Jasnoor <jasnoorsingh0902@gmail.com>
2023-05-13 01:18:08 +0530

    fixed images finally

--- src/components/Scitech.vue
+++ src/components/Scitech.vue
@@ -11,34 +11,34 @@ export default {
 		<div :class="`content`">
 			<div :class="'links'">
 				<router-link to="/scitech/decoherence">
-					<Card link="/img/scitech/Decoherence.webp" size="300px" />
+					<Card link="/img/scitech/Decoherence.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/bioblitz">
-					<Card link="/img/scitech/BioBlitz.webp" size="300px" />
+					<Card link="/img/scitech/BioBlitz.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/chemenigma">
-					<Card link="/img/scitech/Chemenigma.webp" size="300px" />
+					<Card link="/img/scitech/Chemenigma.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/enumeration">
-					<Card link="/img/scitech/Enumeration.webp" size="300px" />
+					<Card link="/img/scitech/Enumeration.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/quadsparks">
-					<Card link="/img/scitech/Quadsparks.webp" size="300px" />
+					<Card link="/img/scitech/Quadsparks.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/bah">
-					<Card link="/img/scitech/BAH.webp" size="300px" />
+					<Card link="/img/scitech/BAH.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/pis">
-					<Card link="/img/scitech/PIS.webp" size="300px" />
+					<Card link="/img/scitech/PIS.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/exhibitions">
-					<Card link="/img/scitech/Exhibitions.webp" size="300px" />
+					<Card link="/img/scitech/Exhibitions.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/hoco">
-					<Card link="/img/scitech/HonourCode.webp" size="300px" />
+					<Card link="/img/scitech/HonourCode.jpg" size="300px" />
 				</router-link>
 				<router-link to="/scitech/stemposium">
-					<Card link="/img/scitech/Stemposium.webp" size="300px" />
+					<Card link="/img/scitech/Stemposium.jpg" size="300px" />
 				</router-link>
 			</div>
 		</div>
