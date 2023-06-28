579e4be18ca006b69e5d0fb1fbe8289b81140ca5
lo568los <soumyadeepsarma3@gmail.com>
2023-06-28 13:40:43 +0530

    stuff

--- src/components/culturals/elegante.vue
+++ src/components/culturals/elegante.vue
@@ -7,7 +7,7 @@
     <div class = "culturals_navbar">
         <ul class = "ul">
             <li class = 'li'><a href="#news">Gallery</a></li>
-            <li class = 'li'><a href="/pdf/Carvaan_rules.pdf">Rules</a></li>
+            <li class = 'li'><a href="/pdf/Eleganté.pdf">Rules</a></li>
             <li class = 'li'><a href="#contact">Contact</a></li>
         </ul>
     </div>
