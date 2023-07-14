6e2a27218c01a3caa6bc5273ce8f44266033257e
lo568los <soumyadeepsarma3@gmail.com>
2023-07-14 12:16:06 +0530

    popups

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from "./router/index.js"
 
 import vueSweetalert2 from '/node_modules/vue-sweetalert2/dist/vue-sweetalert2.esm.js';
 
-import 'sweetalert2/dist/sweetalert2.min.css';
+import '/node_modules/sweetalert2/dist/sweetalert2.min.css';
 
 
 
